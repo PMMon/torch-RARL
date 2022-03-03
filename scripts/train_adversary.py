50426eed1e3492a24b2196eea8d98a109f6b3f4a
Philipp <philipp.mondorf@googlemail.com>
2022-03-03 14:59:33 +0100

    scripts

---
+++ scripts/train_adversary.py
@@ -0,0 +1,177 @@
+import os, sys
+import argparse
+import difflib
+import gym
+import numpy as np
+import torch 
+
+from stable_baselines3.common.utils import set_random_seed
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+from models.algorithms import ALGOS
+from utils.exp_manager import ExperimentManager
+from utils.utils import StoreDict
+
+# =========================================================================
+#   Training RL agent on environment specified. 
+#   To promote standardization, the script is as close to 
+#   RL Baselines3 Zoo as possible.
+#   (https://github.com/DLR-RM/rl-baselines3-zoo/blob/master/train.py)   
+# ==========================================================================
+
+if __name__ == "__main__": 
+    # Fetch CLI arguments
+    parser = argparse.ArgumentParser("Robust Adversarial RL (RARL)")
+
+    # General configs
+    parser.add_argument("--verbose", type=int, default=0, choices=[0, 1], help="Verbose mode (0: no output, 1: INFO)")
+    parser.add_argument("--seed", type=int, default=-1, help="Random generator seed")
+    parser.add_argument("--num_threads", type=int, default=-1, help="Number of threads for PyTorch")
+
+    # Configs about environment
+    parser.add_argument('--env', type=str, default="BipedalWalker-v3", help='Name of gym environment')
+    parser.add_argument("--n_envs", type=int, default=1, help="Number of environments for stack")
+    parser.add_argument("--vec_env_type", type=str, default="dummy", choices=["dummy", "subproc"], help="VecEnv type")
+    parser.add_argument("--env_kwargs", type=str, nargs="+", action=StoreDict, help="Optional keyword argument to pass to the env constructor")
+
+    # Configs about model
+    parser.add_argument("--algo", type=str, default="ppo", choices=list(ALGOS.keys()), help="RL Algorithm")
+    parser.add_argument("--saved_models_path", type=str, default=os.path.join("saved_models"), help="Path to a saved models")
+    parser.add_argument("--pretrained_model", type=str, default="", help="Path to a pretrained agent to continue training")
+    parser.add_argument("--save_replay_buffer", default=False, action="store_true", help="Save the replay buffer (when applicable)")
+
+    # Configs about hyperparameter
+    parser.add_argument("-params", "--hyperparameter", type=str, nargs="+", action=StoreDict, help="Overwrite hyperparameter (e.g. learning_rate:0.01 train_freq:10)")
+    parser.add_argument("-optimize", "--optimize_hyperparameters", action="store_true", default=False, help="Run hyperparameters search")
+    parser.add_argument("--hyperparameter_path", type=str, default=os.path.join("hyperparameter"), help="Path to a saved models")
+    parser.add_argument("--storage", type=str, default=None, help="Database storage path if distributed optimization should be used")
+    parser.add_argument("--study_name", type=str, default=None, help="Study name for distributed optimization")
+    parser.add_argument("--sampler", type=str, default="tpe", choices=["random", "tpe", "skopt"], help="Sampler to use when optimizing hyperparameters") 
+    parser.add_argument("--pruner", type=str, default="median", choices=["halving", "median", "none"], help="Pruner to use when optimizing hyperparameters")
+    parser.add_argument("--optimization_log_path", type=str, default=os.path.join("hyperparam_optimization"), help="Path to save the evaluation log and optimal policy for "
+                                                                                                                        "each hyperparameter tried during optimization.")
+    parser.add_argument("--n_opt_trials", type=int, default=10, help="Number of trials for optimizing hyperparameters.")
+    parser.add_argument("--no_optim_plots", action="store_true", default=False, help="Disable hyperparameter optimization plots")
+    parser.add_argument("--n_jobs", type=int, default=1, help="Number of parallel jobs when optimizing hyperparameters")
+    parser.add_argument("--n_startup_trials", type=int, default=10, help="Number of trials before using optuna sampler")
+
+    # Configs about training
+    parser.add_argument("-n", "--n_timesteps", type=int, default=-1, help="Number of timesteps")
+    parser.add_argument("--save_freq", type=int, default=-1, help="Save model every k steps (if negative, no checkpoint)")
+    parser.add_argument("--log_interval", type=int, default=-1, help="Override log interval (default: -1, no change)")
+
+    # Configs about evaluation
+    parser.add_argument("--n_evaluations_opt", type=int, default=20, help="Training policies are evaluated every n-timesteps during hyperparameter optimization")
+    parser.add_argument("--n_eval_episodes", type=int, default=5, help="Number of episodes to use for evaluation")
+    parser.add_argument("--n_eval_envs", type=int, default=1, help="Number of environments for evaluation")
+    parser.add_argument("--eval_freq", type=int, default=10000, help="Evaluate the agent every e steps (if negative, no evaluation).")
+
+    # Configs about logging results 
+    parser.add_argument("-tb", "--tensorboard_log", type=str, default=os.path.join("tb_logging"), help="Tensorboard log dir")
+    parser.add_argument("-f", "--log_folder", type=str, default=os.path.join("logging"), help="Log folder")
+
+    # Configs for RARL
+    parser.add_argument('--actor_layers', nargs='+', type=int, default=[100, 100, 100], help='Layer specification for actor')
+    parser.add_argument('--critic_layers', nargs='+', type=int, default=[100, 100, 100], help='Layer specification for critic')
+
+
+
+    parser.add_argument('--path_length', type=int, default=1000, help='maximum episode length')
+    parser.add_argument('--layer_size', nargs='+', type=int, default=[100,100,100], help='layer definition')
+    parser.add_argument('--if_render', type=int, default=0, help='Should we render?')
+    parser.add_argument('--after_render', type=int, default=100, help='After how many to animate')
+    parser.add_argument('--n_exps', type=int, default=1, help='Number of training instances to run')
+    parser.add_argument('--n_itr', type=int, default=25, help='Number of iterations of the alternating optimization')
+    parser.add_argument('--n_pro_itr', type=int, default=1, help='Number of iterations for the portagonist')
+    parser.add_argument('--n_adv_itr', type=int, default=1, help='Number of interations for the adversary')
+    parser.add_argument('--batch_size', type=int, default=4000, help='Number of training samples for each iteration')
+    parser.add_argument('--save_every', type=int, default=100, help='Save checkpoint every save_every iterations')
+    parser.add_argument('--n_process', type=int, default=1, help='Number of parallel threads for sampling environment')
+    parser.add_argument('--adv_fraction', type=float, default=0.25, help='fraction of maximum adversarial force to be applied')
+    parser.add_argument('--step_size', type=float, default=0.01, help='kl step size for TRPO')
+    parser.add_argument('--gae_lambda', type=float, default=0.97, help='gae_lambda for learner')
+    parser.add_argument('--folder', type=str, default=os.environ['HOME'], help='folder to save result in')
+
+    args = parser.parse_args()
+
+    # check gym environment
+    env_id = args.env
+    registered_envs = set(gym.envs.registry.env_specs.keys())
+
+    # if environment is not found, suggest the closest match
+    if env_id not in registered_envs:
+        try:
+            closest_match = difflib.get_close_matches(env_id, registered_envs, n=1)[0]
+        except IndexError:
+            closest_match = "'no close environment match found...'"
+        raise ValueError(f"{env_id} not found in gym registry, did you maybe mean {closest_match}?")
+    
+
+    # set random seed
+    if args.seed < 0:
+        args.seed = np.random.randint(2**32 - 1, dtype="int64").item()
+    
+    set_random_seed(args.seed)
+
+    # set number of threads 
+    if args.num_threads > 0: 
+        if args.verbose == 1: 
+            print(f"Setting torch.num_threads to {args.num_threads}")
+        torch.set_num_threads(args.num_threads)
+    
+    model_path = os.path.join(args.saved_models_path, args.algo.lower(), env_id)
+
+    # check path
+    if args.pretrained_model != "":
+        assert os.path.isfile(os.path.join(model_path, args.pretrained_model, env_id + ".zip")), f"The pretrained_agent must be a valid path to a .zip file. Check path to {os.path.join(model_path, args.pretrained_model)}"
+    
+    # print information
+    print("=" * 10, env_id, "=" * 10)
+    print(f"Seed: {args.seed}")
+
+    # experiment manager
+    exp_manager = ExperimentManager(
+        args, 
+        algo=args.algo,
+        env_id=env_id,
+        log_folder=args.log_folder,
+        tensorboard_log=args.tensorboard_log,
+        n_timesteps=args.n_timesteps,
+        eval_freq=args.eval_freq,
+        n_eval_episodes=args.n_eval_episodes,
+        save_freq=args.save_freq,
+        hyperparameter_path=args.hyperparameter_path,
+        hyperparams=args.hyperparameter,
+        env_kwargs=args.env_kwargs,
+        model_path=model_path,
+        pretrained_model = args.pretrained_model,
+        optimize_hyperparameters=args.optimize_hyperparameters,
+        storage=args.storage,
+        study_name=args.study_name,
+        n_opt_trials=args.n_opt_trials,
+        n_jobs=args.n_jobs,
+        sampler=args.sampler,
+        pruner=args.pruner,
+        optimization_log_path=args.optimization_log_path,
+        n_startup_trials=args.n_startup_trials,
+        n_evaluations_opt=args.n_evaluations_opt,
+        seed=args.seed,
+        log_interval=args.log_interval,
+        save_replay_buffer=args.save_replay_buffer,
+        verbose=args.verbose,
+        vec_env_type=args.vec_env_type,
+        n_envs=args.n_envs,
+        n_eval_envs=args.n_eval_envs,
+        no_optim_plots=args.no_optim_plots
+    )
+
+    # Prepare experiment and launch hyperparameter optimization if needed
+    model = exp_manager.setup_experiment()
+
+    # Normal training
+    if model is not None:
+        exp_manager.learn(model)
+        exp_manager.save_trained_model(model)
+    else:
+        exp_manager.hyperparameters_optimization()
