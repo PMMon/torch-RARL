6dd1f3c42d9589a0a48e7d34875c6b610f754a95
PMMon <philipp.mondorf@googlemail.com>
2022-03-07 16:27:07 +0100

    Wrapper functions

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -1,6 +1,9 @@
 import gym
 from gym import Env, spaces
 import numpy as np
+import torch 
+from stable_baselines3.common.utils import obs_as_tensor
+
 # ================================================
 #   Define and customize gym environment wrapper
 # ================================================
@@ -70,7 +73,7 @@ class AdversarialWrapper(gym.Wrapper):
         self._action_space = self.convert_gym_space(env.action_space, low_val=env.action_space.low, high_val=env.action_space.high)
         self._observation_space = self.convert_gym_space(env.observation_space, low_val=env.observation_space.low, high_val=env.observation_space.high)
         
-    
+
     @property
     def observation_space(self):
         return self._observation_space
@@ -126,6 +129,9 @@ class AdversarialWrapper(gym.Wrapper):
 
 
 class AdversaryRewardWrapper(gym.RewardWrapper):
+    """
+    Adapts the reward function of the adversary in RARL
+    """
     def __init__(self, env):
         super().__init__(env)
     
@@ -133,4 +139,141 @@ class AdversaryRewardWrapper(gym.RewardWrapper):
         # modify rew
         return -rew
 
-        
+
+class AdversarialClassicControlWrapper(gym.Wrapper):
+    """
+    Adapts the action space of the gym environment for the adversary
+    """
+    def __init__(self, env: Env, adv_fraction: float = 1.0):
+        super(AdversarialClassicControlWrapper, self).__init__(env)
+        self.env = env
+        # define adversarial impact
+        adv_magnitude = env.action_space.high[0] * adv_fraction
+        high_adv = np.ones(env.action_space.shape[0], dtype=np.float32) * adv_magnitude
+
+        self._adv_action_space = self.convert_gym_space(env.action_space, low_val=-high_adv, high_val=high_adv)
+        self._action_space = env.action_space #self.convert_gym_space(env.action_space, low_val=env.action_space.low, high_val=env.action_space.high)
+        self._initial_action_space = env.action_space #self.convert_gym_space(env.action_space, low_val=env.action_space.low, high_val=env.action_space.high)
+        self._observation_space = env.observation_space #self.convert_gym_space(env.observation_space, low_val=env.observation_space.low, high_val=env.observation_space.high)
+    
+        self.operating_mode = None
+        self._pro_policy = None
+        self._adv_policy = None
+
+        self.device = "cuda"
+
+    @property
+    def observation_space(self):
+        return self._observation_space
+
+
+    @property
+    def action_space(self):
+        return self._action_space
+    
+
+    @property
+    def initial_action_space(self):
+        return self._initial_action_space
+
+
+    @property
+    def adv_action_space(self):
+        return self._adv_action_space
+
+
+    def set_action_space(self, updated_action_space): 
+        self._action_space = updated_action_space
+
+
+    def set_pro_policy(self, pro_policy):
+        self._pro_policy = pro_policy
+        self._pro_policy.set_training_mode(False)
+    
+
+    def set_adv_policy(self, adv_policy):
+        self._adv_policy = adv_policy
+        self._adv_policy.set_training_mode(False)
+
+
+    def sample_action(self):
+        class pro_adv_action(object):
+            def __init__(self, pro_action, adv_action):
+                self.pro_action = pro_action
+                self.adv_action = adv_action
+
+        return pro_adv_action(self.action_space.sample(), self.adv_action_space.sample())
+
+
+    def step(self, action):
+        if hasattr(action, '__dict__'):
+            print("action has dict")
+            obs, rew, done, info = self.env.step(action + action_sampled)
+            self._old_observation = obs
+            return obs, rew, done, info
+        else:
+            if self.operating_mode.lower() == "protagonist":
+                with torch.no_grad():
+                    # Convert to pytorch tensor or to TensorDict
+                    obs_tensor = obs_as_tensor(self._last_observation, self.device)
+                    if len(obs_tensor.shape) == 1:
+                        obs_tensor = obs_tensor.unsqueeze(0)
+                    action_sampled  = self._adv_policy._predict(obs_tensor, deterministic=True)
+
+                clipped_actions = action_sampled.cpu().numpy()
+
+                # Clip the actions to avoid out of bound error
+                if isinstance(self._adv_action_space, gym.spaces.Box):
+                    clipped_actions = np.clip(clipped_actions, self._adv_action_space.low, self._adv_action_space.high).squeeze(0)
+
+            elif self.operating_mode.lower() == "adversary":
+                with torch.no_grad():
+                    # Convert to pytorch tensor or to TensorDict
+                    obs_tensor = obs_as_tensor(self._last_observation, self.device)
+                    if len(obs_tensor.shape) == 1:
+                        obs_tensor = obs_tensor.unsqueeze(0)
+                    action_sampled = self._pro_policy._predict(obs_tensor, deterministic=True)
+
+                clipped_actions = action_sampled.cpu().numpy()
+
+                # Clip the actions to avoid out of bound error
+                if isinstance(self._action_space, gym.spaces.Box):
+                    clipped_actions = np.clip(clipped_actions, self._action_space.low, self._action_space.high).squeeze(0)
+            else: 
+                raise ValueError(f"Please choose operating mode either 'protagonist' or 'adversary', not: ", self.operating_mode)
+
+            obs, rew, done, info = self.env.step(action) # + clipped_actions)
+            self._last_observation = obs
+            return obs, rew, done, info
+    
+
+    def reset(self):
+        self._last_observation = self.env.reset()
+        return self._last_observation
+
+
+    def render(self):
+        self.env.render()
+
+
+    def convert_gym_space(self, space, low_val, high_val):
+        """Converts gym space into appropriate categories
+
+        Args:
+            space (gym.spaces): gym space
+
+        Returns:
+            gym.spaces: converted gym space
+        """
+        if isinstance(space, gym.spaces.Box):
+            return spaces.box.Box(low=low_val, high=high_val)
+        elif isinstance(space, gym.spaces.Discrete):
+            return spaces.discrete.Discrete(n=space.n)
+        elif isinstance(space, gym.spaces.Tuple):
+            return spaces.prodcut.Product([self.convert_gym_space(x) for x in space.spaces])
+        else:
+            raise NotImplementedError
+
+
+
+
