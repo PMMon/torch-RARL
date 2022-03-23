1166b780f927860d3e88611eb5cf686fc324f147
PMMon <philipp.mondorf@googlemail.com>
2022-03-23 11:11:37 +0100

    utils

--- utils/exp_manager.py
+++ utils/exp_manager.py
@@ -1,4 +1,3 @@
-from gc import callbacks
 import os, sys
 import argparse
 import yaml
@@ -100,6 +99,7 @@ class ExperimentManager(object):
         adv_force_dim: int = 2,
         total_steps_protagonist: int = 10, 
         total_steps_adversary: int = 10,
+        device: str = None
     ) -> None:
         super(ExperimentManager, self).__init__()
         self.args = args
@@ -136,6 +136,7 @@ class ExperimentManager(object):
         # training
         self.n_timesteps = n_timesteps
         self.save_freq = save_freq
+        self.device = device
 
         # evaluation
         self.n_eval_episodes = n_eval_episodes
@@ -169,20 +170,18 @@ class ExperimentManager(object):
 
         # logging
         self.verbose = verbose
-        self.log_folder = log_folder
-        self.log_path = os.path.join(log_folder, self.algo)
         self.tensorboard_log = None if tensorboard_log == "" else os.path.join(tensorboard_log, env_id)
         self.log_interval = log_interval
 
         # paths
         self.model_path = model_path
         if pretrained_model == "":
-            self.save_path = os.path.join(self.model_path, f"{get_latest_run_id(self.model_path, self.env_id) + 1}")
+            self.save_path = os.path.join(self.model_path, f"{self.env_id}_{get_latest_run_id(self.model_path, self.env_id) + 1}")
             self.continue_training = False
         else: 
             self.save_path = os.path.join(self.model_path, pretrained_model)
             self.continue_training = True
-        self.params_path = os.path.join(self.save_path, "params")
+        self.params_path = os.path.join(self.save_path, self.env_id)
 
         # RARL
         self.adv_delay = adv_delay
@@ -346,6 +345,10 @@ class ExperimentManager(object):
             self.specified_callbacks = hyperparams["callback"]
             del hyperparams["callback"]
 
+        # manage devide
+        if "device" in hyperparams.keys():
+            self.device = hyperparams["device"]
+
         return hyperparams, env_wrapper, callbacks
     
 
@@ -435,7 +438,7 @@ class ExperimentManager(object):
                 callback_on_new_best=save_vec_normalize,
                 best_model_save_path=self.save_path,
                 n_eval_episodes=self.n_eval_episodes,
-                log_path=self.log_path,
+                log_path=self.save_path,
                 eval_freq=self.eval_freq,
                 deterministic=self.deterministic_eval,
             )
@@ -468,9 +471,13 @@ class ExperimentManager(object):
             if self.adv_impact.lower() == "control":
                 self.env_wrapper = AdversarialClassicControlWrapper
                 wrapper_kwargs.update(dict(adv_fraction=self.adv_fraction))
+                if self.device:
+                    wrapper_kwargs.update(dict(device=self.device))
             elif self.adv_impact.lower() == "force": 
                 self.env_wrapper = AdversarialMujocoWrapper
                 wrapper_kwargs.update(dict(adv_low=self.adv_low, adv_high=self.adv_high, index_list=self.adv_index_list, force_dim=self.adv_force_dim))
+                if self.device:
+                    wrapper_kwargs.update(dict(device=self.device))
         else: 
             wrapper_kwargs = {}
 
--- utils/utils.py
+++ utils/utils.py
@@ -3,6 +3,7 @@ import glob
 import importlib
 import gym
 import argparse
+import yaml
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 from stable_baselines3.common.callbacks import BaseCallback
@@ -237,10 +238,10 @@ def get_latest_run_id(log_path: str, env_id: str) -> int:
     copies or substantial portions of the Software.
     """
     max_run_id = 0
-    for path in glob.glob(os.path.join(log_path, "[0-9]*")):
+    for path in glob.glob(os.path.join(log_path, env_id + "_[0-9]*")):
         file_name = os.path.basename(path)
-        ext = file_name
-        if ext.isdigit() and int(ext) > max_run_id:
+        ext = file_name.split("_")[-1]
+        if env_id == "_".join(file_name.split("_")[:-1]) and ext.isdigit() and int(ext) > max_run_id:
             max_run_id = int(ext)
     return max_run_id
 
@@ -281,3 +282,58 @@ class StoreDict(argparse.Action):
             # Evaluate the string as python code
             arg_dict[key] = eval(value)
         setattr(namespace, self.dest, arg_dict)
+
+
+def get_saved_hyperparams(
+    stats_path: str,
+    norm_reward: bool = False,
+    test_mode: bool = False,
+) -> Tuple[Dict[str, Any], str]:
+    """
+    Loads saved hyperparameters from .config file
+    :param stats_path: path to config file
+    :param norm_reward: reward normalization
+    :param test_mode: collect configs for norming reward
+    :return: hyperparameters, updated stats_path
+
+    Taken from RL Baselines3 Zoo 
+    <https://github.com/DLR-RM/rl-baselines3-zoo/blob/master/utils/utils.py>
+
+    MIT License
+
+    Copyright (c) 2019 Antonin RAFFIN
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    """
+    hyperparams = {}
+    if not os.path.isdir(stats_path):
+        stats_path = None
+    else:
+        config_file = os.path.join(stats_path, "config.yml")
+        if os.path.isfile(config_file):
+            # Load saved hyperparameters
+            with open(os.path.join(stats_path, "config.yml"), "r") as f:
+                hyperparams = yaml.load(f, Loader=yaml.UnsafeLoader)  # pytype: disable=module-attr
+            hyperparams["normalize"] = hyperparams.get("normalize", False)
+        else:
+            obs_rms_path = os.path.join(stats_path, "obs_rms.pkl")
+            hyperparams["normalize"] = os.path.isfile(obs_rms_path)
+
+        # Load normalization params
+        if hyperparams["normalize"]:
+            if isinstance(hyperparams["normalize"], str):
+                normalize_kwargs = eval(hyperparams["normalize"])
+                if test_mode:
+                    normalize_kwargs["norm_reward"] = norm_reward
+            else:
+                normalize_kwargs = {"norm_obs": hyperparams["normalize"], "norm_reward": norm_reward}
+            hyperparams["normalize_kwargs"] = normalize_kwargs
+    return hyperparams, stats_path
